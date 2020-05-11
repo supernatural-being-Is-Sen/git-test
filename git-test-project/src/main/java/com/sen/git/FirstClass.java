5e2ecb6394817118d9feb87d843bc0de2fb4f7d4
supernatural-being-Is-Sen <1214166597@qq.com>
2020-05-11 21:16:12 +0800

    使用 idea 将工程推送到本地库

---
+++ git-test-project/src/main/java/com/sen/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.sen.git;
+
+/**
+ * @author 吴雨森
+ * @data 2020/5/11
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("git 牛逼");
+        System.out.println("git 牛逼2");
+        System.out.println("git 牛逼3");
+    }
+}
